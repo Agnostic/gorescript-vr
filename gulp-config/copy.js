a8c1064bff5e8ab6c9e3d7a16810fefa5885a335
Gilberto Avalos <gilberto@ooyala.com>
2016-02-09 10:40:51 -0600

    First demo

--- gulp-config/copy.js
+++
@@ -1,9 +0,0 @@
-var gulp = require("gulp");
-
-gulp.task("copy", function () {
-	return gulp.src([
-			"./src/game/index.html",
-			"./src/game/assets/fonts/capsuula.woff"
-		])
-		.pipe(gulp.dest(global.distFolder));
-});
